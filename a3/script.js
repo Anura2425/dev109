f60c824647b7745c37fd0774006fcbb10611deeb
Gavin McClure-Coleman <150880291+Anura2425@users.noreply.github.com>
2024-05-11 23:38:22 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,19 +1,60 @@
-function drawRhombus() {
-     const height = document.getElementById('height').value;
-     const oddColor = document.getElementById('odd-color').value;
-     const evenColor = document.getElementById('even-color').value;
-     const symbol = document.getElementById('symbol').value;
+const form = document.getElementById('rhombus-form');
+        const output = document.getElementById('rhombus-output');
 
-     let rhombus = '';
-     for (let i = 0; i < height; i++) {
-       const line = symbol.repeat(i + 1);
-       rhombus += `<span style="color:${i % 2 === 0 ? evenColor : oddColor}">${line}</span><br>`;
-     }
+        function generateRhombus(height, oddColor, evenColor, symbol) {
+            let rhombus = '';
+            const maxWidth = height * 2 - 1;
 
-     for (let i = height - 2; i >= 0; i--) {
-       const line = symbol.repeat(i + 1);
-       rhombus += `<span style="color:${i % 2 === 0 ? evenColor : oddColor}">${line}</span><br>`;
-     }
+            for (let i = 0; i < height; i++) {
+                let line = '';
+                const spaces = maxWidth - 2 * i;
+                const symbols = i + 1;
 
-     document.getElementById('rhombus').innerHTML = rhombus;
-   }
+                for (let j = 0; j < spaces / 2; j++) {
+                    line += ' ';
+                }
+
+                for (let j = 0; j < symbols; j++) {
+                    line += `<span style="color: ${(j + i) % 2 === 0 ? evenColor : oddColor};">${symbol}</span>`;
+                }
+
+                for (let j = 0; j < spaces / 2; j++) {
+                    line += ' ';
+                }
+
+                rhombus += line + '\n';
+            }
+
+            for (let i = height - 2; i >= 0; i--) {
+                let line = '';
+                const spaces = maxWidth - 2 * i;
+                const symbols = i + 1;
+
+                for (let j = 0; j < spaces / 2; j++) {
+                    line += ' ';
+                }
+
+                for (let j = 0; j < symbols; j++) {
+                    line += `<span style="color: ${(j + i) % 2 === 0 ? evenColor : oddColor};">${symbol}</span>`;
+                }
+
+                for (let j = 0; j < spaces / 2; j++) {
+                    line += ' ';
+                }
+
+                rhombus += line + '\n';
+            }
+
+            return rhombus;
+        }
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const height = parseInt(form.elements.height.value);
+            const oddColor = form.elements['odd-color'].value;
+            const evenColor = form.elements['even-color'].value;
+            const symbol = form.elements.symbol.value;
+
+            output.innerHTML = `<pre>${generateRhombus(height, oddColor, evenColor, symbol)}</pre>`;
+        });
