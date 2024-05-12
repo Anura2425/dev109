27e39c5a16432af7b75fe7ad29847a6bf671948f
Gavin McClure-Coleman <150880291+Anura2425@users.noreply.github.com>
2024-05-12 07:15:47 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,7 +1,57 @@
 const form = document.getElementById('rhombus-form');
         const output = document.getElementById('rhombus-output');
 
-        function generateRhombus(height, oddColor, evenColor, symbol) {
+function createRightRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+
+}
+
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+for(j=0;j<=i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+
+}
+
+document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downRight").innerHTML = rLine;
+}
+
+        function createLeftRhombus(height, oddColor, evenColor, symbol) {
             let rhombus = '';
             const maxWidth = height * 2 - 1;
 
@@ -56,5 +106,5 @@ const form = document.getElementById('rhombus-form');
             const evenColor = form.elements['even-color'].value;
             const symbol = form.elements.symbol.value;
 
-            output.innerHTML = `<pre>${generateRhombus(height, oddColor, evenColor, symbol)}</pre>`;
+            output.innerHTML = `<pre>${createLeftRhombus(height, oddColor, evenColor, symbol)}</pre>`;
         });
