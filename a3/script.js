a6027f56087b07e3c559d69c7943d8bd40ab7af1
Gavin McClure-Coleman <150880291+Anura2425@users.noreply.github.com>
2024-05-11 23:32:06 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,28 +1,19 @@
-function generateRhombus(height, oddColor, evenColor, symbol) {
-  let rhombus = '';
-  const maxWidth = height * 2 - 1;
+function drawRhombus() {
+     const height = document.getElementById('height').value;
+     const oddColor = document.getElementById('odd-color').value;
+     const evenColor = document.getElementById('even-color').value;
+     const symbol = document.getElementById('symbol').value;
 
-  // Top part
-  for (let i = 0; i < height; i++) {
-    let line = '';
-    for (let j = 0; j < maxWidth; j++) {
-      const color = (i + j) % 2 === 0 ? evenColor : oddColor;
-      const char = j >= height - i - 1 && j < height + i ? symbol : ' ';
-      line += `<span style="color: ${color};">${char}</span>`;
-    }
-    rhombus += line + '<br>';
-  }
+     let rhombus = '';
+     for (let i = 0; i < height; i++) {
+       const line = symbol.repeat(i + 1);
+       rhombus += `<span style="color:${i % 2 === 0 ? evenColor : oddColor}">${line}</span><br>`;
+     }
 
-  // Bottom part
-  for (let i = height - 2; i >= 0; i--) {
-    let line = '';
-    for (let j = 0; j < maxWidth; j++) {
-      const color = (i + j) % 2 === 0 ? evenColor : oddColor;
-      const char = j >= height - i - 1 && j < height + i ? symbol : ' ';
-      line += `<span style="color: ${color};">${char}</span>`;
-    }
-    rhombus += line + '<br>';
-  }
+     for (let i = height - 2; i >= 0; i--) {
+       const line = symbol.repeat(i + 1);
+       rhombus += `<span style="color:${i % 2 === 0 ? evenColor : oddColor}">${line}</span><br>`;
+     }
 
-  return rhombus;
-}
+     document.getElementById('rhombus').innerHTML = rhombus;
+   }
